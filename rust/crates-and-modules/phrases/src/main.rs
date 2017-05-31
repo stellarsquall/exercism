eb8609d6a03dc7c93fb41b176ac151834c5b7349
Robin Beck <robin.a.beck@gmail.com>
2017-05-31 00:48:00 -0600

    tutorial finished - although demo for complex imports breaks cargo build

--- rust/crates-and-modules/phrases/src/main.rs
+++ rust/crates-and-modules/phrases/src/main.rs
@@ -1,12 +1,30 @@
-extern crate phrases;
+// tutorial through Re-exporting with pub use
 
-use phrases::english::{greetings,farewells};
-use phrases::japanese;
+//extern crate phrases;
+//
+//use phrases::english::{greetings,farewells};
+//use phrases::japanese;
+//
+//fn main() {
+//    println!("Hello in English: {}", greetings::hello());
+//    println!("Goodbye in English: {}", farewells::goodbye());
+//
+//    println!("Hello in Japanese: {}", japanese::hello());
+//    println!("Goodbye in Japanese: {}", japanese::goodbye());
+//}
+
+// Complex imports
+
+extern crate phrases as sayings;
+
+use sayings::japanese::greetings as ja_greetings;
+use sayings::japanese::farewells::*;
+use sayings::english::{self, greetings as en_greetings, farewells as en_farewells};
 
 fn main() {
-    println!("Hello in English: {}", greetings::hello());
-    println!("Goodbye in English: {}", farewells::goodbye());
-
-    println!("Hello in Japanese: {}", japanese::hello());
-    println!("Goodbye in Japanese: {}", japanese::goodbye());
+    println!("Hello in English; {}", en_greetings::hello());
+    println!("And in Japanese: {}", ja_greetings::hello());
+    println!("Goodbye in English: {}", english::farewells::goodbye());
+    println!("Again: {}", en_farewells::goodbye());
+    println!("And in Japanese: {}", goodbye());
 }
---
+++ rust/crates-and-modules/phrases/target/debug/deps/phrases-8eeedc4da00ea585.d
@@ -0,0 +1,5 @@
+/Users/robinabeck/exercism/rust/crates-and-modules/phrases/target/debug/deps/phrases-8eeedc4da00ea585: src/main.rs
+
+/Users/robinabeck/exercism/rust/crates-and-modules/phrases/target/debug/deps/phrases-8eeedc4da00ea585.d: src/main.rs
+
+src/main.rs:
